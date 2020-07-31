045b3eaed5b365d91f62c9e4c0a55b9ac45940e9
Stephen Junge <steve.junge@gmail.com>
2020-07-31 20:41:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,12 +7,15 @@ def countdown(seconds_to_midnight)
       puts "#{seconds_to_midnight} SECOND(S)!"
       seconds_to_midnight -= 1
   end      
-  
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_midnight)
   while seconds_to_midnight > 0
-    sleep 1
+   puts "#{seconds_to_midnight} SECOND(S)!"
+      seconds_to_midnight -= 1
+      sleep 1
   end
   
 return "HAPPY NEW YEAR!"
-
-   
 end
