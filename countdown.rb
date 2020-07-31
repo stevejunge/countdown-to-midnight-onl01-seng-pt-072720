b53e2be11b7d5041be928e1ad9f2d8991dbd1574
Stephen Junge <steve.junge@gmail.com>
2020-07-31 01:31:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,18 +2,17 @@
 
    
 
-def countdown_with_sleep(number)
-  while number > 0
-      puts "#{number} SECOND(S)!"
-      number -= 1
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+      puts "#{seconds_to_midnight} SECOND(S)!"
+      seconds_to_midnight -= 1
   end      
   
-  while number > 0
+  while seconds_to_midnight > 0
     sleep 1
   end
   
 return "HAPPY NEW YEAR!"
 
    
-end
 end
